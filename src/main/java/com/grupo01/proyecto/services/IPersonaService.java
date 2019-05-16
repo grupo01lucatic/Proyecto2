df34b2566fdecd753ce532f30cc0acb6a553deb0
Santi <santialovera20@gmail.com>
2019-05-16 10:37:09 +0200

    2.d Metodo listar contactos en servicios y correcciones

--- src/main/java/com/grupo01/proyecto/services/IPersonaService.java
+++ src/main/java/com/grupo01/proyecto/services/IPersonaService.java
@@ -1,17 +1,13 @@
 package com.grupo01.proyecto.services;
 
-import java.util.List;
-
 import com.grupo01.proyecto.model.Persona;
 
-
-
 public interface IPersonaService {
-	public List<Persona> findAll();
-	
+	public Iterable<Persona> findAll();
+
 	public void save(Persona persona);
-	
+
 	public Persona findOne(Long id);
-	
+
 	public void delete(Long id);
 }
