79f214f5e4b39bc49a5b64a4c5746d3c674d58dc
icarpio <ivancg24@gmail.com>
2019-05-16 12:48:12 +0200

    Modifcacion de Entitys, metodo guardar, crear del controlador

--- src/main/java/com/grupo01/proyecto/model/Provincia.java
+++ src/main/java/com/grupo01/proyecto/model/Provincia.java
@@ -1,9 +1,13 @@
 package com.grupo01.proyecto.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
 1  * Nombre: Provincia
@@ -14,23 +18,33 @@ import javax.persistence.Id;
 */
 
 @Entity
-public class Provincia {
+@Table(name="provincias")
+public class Provincia implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int idprovincia;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "idprovincia")
+	private Long id;
+	
 	private String provincia;
+	
 
-	public int getIdprovincia() {
-		return idprovincia;
+	public Long getId() {
+		return id;
 	}
 
-	public void setIdprovincia(int idprovincia) {
-		this.idprovincia = idprovincia;
+
+	public void setId(Long id) {
+		this.id = id;
 	}
+
 
 	public String getProvincia() {
 		return provincia;
 	}
+
 
 	public void setProvincia(String provincia) {
 		this.provincia = provincia;
@@ -38,7 +52,7 @@ public class Provincia {
 
 	@Override
 	public String toString() {
-		return "Provincia [idprovincia=" + idprovincia + ", provincia=" + provincia + "]";
+		return "Provincia [idprovincia=" + id + ", provincia=" + provincia + "]";
 	}
 
 }
