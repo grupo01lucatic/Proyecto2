4541ffa07b9b69803bfbf19c102e76c92928bad4
Jara Dominguez <jaradominguezavila@gmail.com>
2019-05-16 16:57:34 +0200

    6.g Creado interface de ProvinciasDao

--- src/main/java/com/grupo01/proyecto/services/ProvinciaServiceImpl.java
+++ src/main/java/com/grupo01/proyecto/services/ProvinciaServiceImpl.java
@@ -1,21 +1,39 @@
 package com.grupo01.proyecto.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.grupo01.proyecto.dao.ProvinciaCrud;
 import com.grupo01.proyecto.model.Provincia;
+
+/**
+ * @author Grupo01
+ * @date 16.05.2019
+ * @version v1
+ */
 
 @Repository
 public class ProvinciaServiceImpl implements IProvinciaService {
-
+	@Autowired
+	private ProvinciaCrud provinciacrud;
+	
 	@Override
 	public Iterable<Provincia> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	/**
+	 * @author Jara Dominguez
+	 * @date 16.05.2019
+	 * @param Provincia
+	 * @return void
+	 * Metodo que recibe un objeto tipo Provincia y lo guarda en la base de datos a
+	 * traves de l implimentacion del metodo save de CrudRepository
+	 */
+	
 	@Override
 	public void save(Provincia provincia) {
-		// TODO Auto-generated method stub
+		provinciacrud.save(provincia);
 		
 	}
 
