b7d08a68d8bb8e231b0d436cea904ca1cfe302f1
Santi <santialovera20@gmail.com>
2019-05-21 15:46:01 +0200

    15.a RestController eliminar contacto

--- src/main/java/com/grupo01/proyecto/controller/JsonController.java
+++ src/main/java/com/grupo01/proyecto/controller/JsonController.java
@@ -3,7 +3,9 @@ package com.grupo01.proyecto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,4 +36,17 @@ public class JsonController {
 	public List<Persona> read() {
 		return servicios.findAll();
 	}
+	
+	/**
+	 * Servicio REST para eliminar un contacto
+	 * 
+	 * @author Santiago Villar
+	 * @date 21.05.2019
+	 * @return void
+	 */
+	@DeleteMapping("/eliminarcontacto{id}")
+	public void delete(@PathVariable int id) {
+		servicios.delete(id);
+	}
+	
 }
